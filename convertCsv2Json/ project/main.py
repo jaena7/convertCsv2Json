586a4dc93ebfc64f0862aa5f7c2ec0ea9db66878
Jana Němcová <jana.nemcova@direct.cz>
2024-01-25 20:25:15 +0100

    Add user input

--- convertCsv2Json/ project/main.py
+++ convertCsv2Json/ project/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from config_utils import read_config, get_encoding, get_delimiter
 from files_utils import read_csv, process_data, write_json
@@ -8,12 +9,28 @@ if __name__ == "__main__":
     try:
         setup_logging()
         config = read_config()
-        csv_file_path = config.get('path', 'csv_file')
-        json_file_path = config.get('path', 'json_file')
 
-        header, csv_data = read_csv(csv_file_path, get_delimiter(config))
+        def_csv_file_path = config.get('path', 'csv_file')
+        csv_file_path = input(
+            f"Enter the input path to the CSV file (default is '{def_csv_file_path}'): ") or def_csv_file_path
+        csv_file_path = os.path.abspath(os.path.normpath(csv_file_path))
+
+        def_json_file_path = config.get('path', 'json_file')
+        json_file_path = input(
+            f"Enter the output path to the JSON file (default is '{def_json_file_path}'): ") or def_json_file_path
+        json_file_path = os.path.normpath(json_file_path)
+
+        def_delimiter = get_delimiter(config)
+        delimiter = input(
+            f"Enter the CSV delimiter (default is '{def_delimiter}'): ") or def_delimiter
+
+        def_encoding = get_encoding(config)
+        encoding = input(
+            f"Enter the JSON encoding (default is '{def_encoding}'): ") or def_encoding
+
+        header, csv_data = read_csv(csv_file_path, delimiter)
         json_data = process_data(header, csv_data)
-        write_json(json_data, json_file_path, get_encoding(config))
+        write_json(json_data, json_file_path, encoding)
 
         logging.info("Conversion completed successfully.")
     except Exception as e:
